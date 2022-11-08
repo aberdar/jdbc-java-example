000e76c994dd0c01ed0e37ea3b670abb16d4acf1
aberdar <sashaberdar@icloud.com>
2022-11-08 16:10:21 +0300

    Adding create table method

--- src/main/java/ru/ac/uniyar/databasescourse/tables/SolutionsTable.java
+++ src/main/java/ru/ac/uniyar/databasescourse/tables/SolutionsTable.java
@@ -1,2 +1,31 @@
-package ru.ac.uniyar.databasescourse.tables;public class SolutionsTable {
+package ru.ac.uniyar.databasescourse.tables;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class SolutionsTable {
+    private static final String tableName = "solutions",
+                                studentTable = "students",
+                                reviewerTable = "reviewers";
+
+    public static void createTable(Connection connection) throws SQLException {
+        Statement statement = connection.createStatement();
+        statement.execute(
+                "CREATE TABLE IF NOT EXISTS "+ tableName +
+                        "(" +
+                        "solutionID INT PRIMARY KEY," +
+                        "studentID INT NOT NULL," +
+                        "reviewerID INT NOT NULL," +
+                        "score DOUBLE(2,1) NOT NULL," +
+                        "hasPass ENUM('Yes', 'No')," +
+                        "FOREIGN KEY (studentID) REFERENCES " + studentTable + "(studentID)," +
+                        "FOREIGN KEY (reviewerID) REFERENCES " + reviewerTable + "(reviewerID)" +
+                        ");"
+        );
+
+        statement.close();
+    }
+
+    public static void insertTable() {}
 }
