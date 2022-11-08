28187f5e5e671dbf6361f2ef1d43308d9240056d
aberdar <sashaberdar@icloud.com>
2022-11-08 15:50:55 +0300

    Change department class

--- src/main/java/ru/ac/uniyar/databasescourse/storage/Department.java
+++ src/main/java/ru/ac/uniyar/databasescourse/storage/Department.java
@@ -1,2 +1,22 @@
-package ru.ac.uniyar.databasescourse.storage;public class ReviewerDepartment {
+package ru.ac.uniyar.databasescourse.storage;
+
+public class Department {
+    private final Integer id;
+    private final String name;
+
+    public Department(
+            Integer id,
+            String name
+    ) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
