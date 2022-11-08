be02060a12a87b2324d8a8d67b33883b587de934
aberdar <sashaberdar@icloud.com>
2022-11-08 15:49:19 +0300

    Methods for creating departments table and insert data

--- src/main/java/ru/ac/uniyar/databasescourse/tables/DepartmentsTable.java
+++ src/main/java/ru/ac/uniyar/databasescourse/tables/DepartmentsTable.java
@@ -1,15 +1,38 @@
-package ru.ac.uniyar.databasescourse.query;
+package ru.ac.uniyar.databasescourse.tables;
+
+import ru.ac.uniyar.databasescourse.storage.Department;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.HashSet;
 
-public class ReviewerDepartmentsTable {
+public class DepartmentsTable {
+    private static final String tableName = "departments";
+    private static final String referencesTableName = "reviewers";
 
     public static void createTable(Connection connection) throws SQLException {
         Statement statement = connection.createStatement();
         statement.execute(
-                "CREATE TABLE departments"
-        )
+                "CREATE TABLE IF NOT EXISTS " + tableName +
+                        "(" +
+                        "reviewerID INT NOT NULL," +
+                        "departmentName VARCHAR(50) NOT NULL," +
+                        "FOREIGN KEY (reviewerID) REFERENCES " + referencesTableName + "(reviewerID)" +
+                        ");"
+        );
+        statement.close();
+    }
+
+    public static void insertData(Connection connection, HashSet<Department> departments) throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement(
+                "INSERT INTO " + tableName +" (reviewerID, departmentName) values (?, ?)"
+        );
+
+        for (Department department: departments) {
+            preparedStatement.setInt(1, department.getId());
+            preparedStatement.setString(2, department.getName());
+        }
     }
 }
