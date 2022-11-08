c58a32d6c47d0a5ec0affd8df61f955d4782e2b7
aberdar <sashaberdar@icloud.com>
2022-11-08 16:09:20 +0300

    Change solution class

--- src/main/java/ru/ac/uniyar/databasescourse/storage/Solution.java
+++ src/main/java/ru/ac/uniyar/databasescourse/storage/Solution.java
@@ -1,2 +1,43 @@
-package ru.ac.uniyar.databasescourse.storage;public class Solution {
+package ru.ac.uniyar.databasescourse.storage;
+
+public class Solution {
+    private final Integer solutionID;
+    private final Integer studentID;
+    private final Integer reviewerID;
+    private final Double score;
+    private final String hasPass;
+
+    public Solution(
+            Integer solutionID,
+            Integer studentID,
+            Integer reviewerID,
+            Double score,
+            String hasPass
+    ) {
+        this.solutionID = solutionID;
+        this.studentID = studentID;
+        this.reviewerID = reviewerID;
+        this.score = score;
+        this.hasPass = hasPass;
+    }
+
+    public Integer getSolutionID() {
+        return solutionID;
+    }
+
+    public Integer getStudentID() {
+        return studentID;
+    }
+
+    public Integer getReviewerID() {
+        return reviewerID;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public String getHasPass() {
+        return hasPass;
+    }
 }
