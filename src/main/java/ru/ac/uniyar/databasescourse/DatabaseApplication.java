fc2d8b638afc58229d1b759bec9533ba29f4779f
aberdar <sashaberdar@icloud.com>
2022-11-10 00:27:36 +0300

    Rename file

--- src/main/java/ru/ac/uniyar/databasescourse/DatabaseApplication.java
+++ src/main/java/ru/ac/uniyar/databasescourse/DatabaseApplication.java
@@ -1,16 +1,6 @@
 package ru.ac.uniyar.databasescourse;
 
-import ru.ac.uniyar.databasescourse.storage.Student;
-import ru.ac.uniyar.databasescourse.utils.SomeCsvDataLoader;
-
-import java.io.IOException;
-import java.sql.*;
-import java.util.List;
-
 public class DatabaseExample {
-    private static final String URL = String.format("jdbc:mariadb://%s", System.getenv("MARIADB_HOST"));
-    private static final String user = System.getenv("MARIADB_USER");
-    private static final String password = System.getenv("MARIADB_PASSWORD");
 
     public static void main(String[] args) {
         System.out.println("The work has started");
@@ -37,10 +27,6 @@ public class DatabaseExample {
 //        } catch (SQLException ex) {
 //            System.out.printf("Can't create connection: %s\n", ex);
 //        }
-    }
-
-    private static Connection createConnection() throws SQLException {
-        return DriverManager.getConnection(URL, user, password);
     }
 }
 
